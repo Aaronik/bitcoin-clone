5c583beb2c26eef8bfebf0317300e477ecad7e80
Aaron Sullivan <aasullivan1618@gmail.com>
2018-06-13 16:22:50 -0700

    Get blocks up and running (but without correct nonce)

--- coinGenerator.js
+++ coinGenerator.js
@@ -1,11 +1,50 @@
+// Globals
+const cryptoUtils = require('crypto-utils');
+
 const fileUtil = require('./file-util');
 
+const config = fileUtil.readConfig(); // contents of our config
+
+// the main funk
 const main = () => {
 
   // first we deal with the wallet file
   if (!fileUtil.walletFileExists()) {
     fileUtil.createWallet();
   }
+
+  // TODO optimize by returning from fileUtil.createWallet
+  // (or don't optimize b/c this will not be a bottleneck)
+  const wallet = fileUtil.readWallet();
+
+  // now let's create that reward tx
+  const rewardTx = {
+    inputs: [], // empty for _reward_ tx
+    outputs: [{
+      address: wallet.pk,
+      value: config.blockReward
+    }],
+    nonce: cryptoUtils.randomBits()
+  };
+
+  // and now the block containing the reward tx
+  const blockHeader = {
+    hashPrevHeader: (0).toString(16).repeat(32),
+    hashTxs: cryptoUtils.hash(rewardTx),
+    bits: config.difficultyLevel,
+    nonce: cryptoUtils.randomBits(),
+  };
+
+  const block = {
+    header: blockHeader,
+    txs: [{ transaction: rewardTx }],
+    signer: wallet.pk,
+    sig: cryptoUtils.sign({ header: blockHeader }, wallet.sk),
+    height: 0 // TODO this OK?
+  };
+
+  console.log(block);
+
 };
 
 main();
--- file-util.js
+++ file-util.js
@@ -1,19 +1,20 @@
 // global declarations
 const fs = require('fs');
+const cryptoUtils = require('crypto-utils');
 
-const { generateKeypair } = require('crypto-utils');
-
-const WALLET_FILE_NAME = './wallet.json'; // where's that wallet being saved
+const WALLET_PATH = './wallet.json'; // where's that wallet being saved
+const CONFIG_PATH = './config.json'; // what's the name of the config file
 
 // public functions
 
 // returns a bool of whether a wallet file exists
 const walletFileExists = () => {
-  return _fileExists(WALLET_FILE_NAME);
+  return _fileExists(WALLET_PATH);
 };
 
+// creates `WALLET_PATH` and populates it
 const createWallet = () => {
-  const keyPair = generateKeypair();
+  const keyPair = cryptoUtils.generateKeypair();
 
   const contents = {
     name: 'miner',
@@ -23,13 +24,26 @@ const createWallet = () => {
 
   const writeData = JSON.stringify(contents);
 
-  fs.writeFileSync(WALLET_FILE_NAME, writeData, { encoding: 'utf8' });
+  fs.writeFileSync(WALLET_PATH, writeData, { encoding: 'utf8' });
+};
+
+// reads the config from disk and returns an object of its contents
+const readConfig = () => {
+  return _readFile(CONFIG_PATH);
+};
+
+const readWallet = () => {
+  return _readFile(WALLET_PATH);
 };
 
 // private helper functions
 
 const _fileExists = (path) => {
-  return fs.existsSync(path);
+  return JSON.parse(fs.existsSync(path));
 };
 
-module.exports = { walletFileExists, createWallet };
+const _readFile = (path) => {
+  return JSON.parse(fs.readFileSync(path, { encoding: 'utf8' }));
+};
+
+module.exports = { walletFileExists, createWallet, readConfig, readWallet };
