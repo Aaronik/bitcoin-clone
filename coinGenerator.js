015449f2dd3f44b6a33ac1afaed0637280de947b
Aaron Sullivan <aasullivan1618@gmail.com>
2018-06-14 14:29:33 -0700

    move mining to miner.js

--- coinGenerator.js
+++ coinGenerator.js
@@ -1,30 +1,11 @@
 // Globals
-const cryptoUtils = require('crypto-utils')
+const WALLET_PATH = './wallet.json' // where's that wallet being saved
+const CONFIG_PATH = './server-config.json' // what's the name of the config file
 
-const hashUtil = require('./hash-util')
-const fileUtil = require('./file-util')
+const fileUtil = require('./file-util')({ CONFIG_PATH, WALLET_PATH })
+const miner = require('./miner')
 
 const config = fileUtil.readConfig() // contents of our config
-
-const mineNewBlock = (pk, sk, rewardTx, hashPrevHeader) => {
-  let blockHeader = {
-    hashPrevHeader: hashPrevHeader,
-    hashTxs: cryptoUtils.hash(rewardTx),
-    bits: config.difficultyLevel
-  }
-
-  blockHeader.nonce = hashUtil.generateProofOfWork(blockHeader, Number(config.difficultyLevel))
-
-  const block = {
-    header: blockHeader,
-    txs: [rewardTx],
-    signer: pk,
-    sig: cryptoUtils.sign({ header: blockHeader }, sk),
-    height: 0 // TODO this OK?
-  }
-
-  return block
-}
 
 // the main funk
 const main = () => {
@@ -37,28 +18,14 @@ const main = () => {
   // (or don't optimize b/c this will not be a bottleneck)
   const wallet = fileUtil.readWallet()
 
-  // now let's create that reward tx
-  const rewardTx = {
-    inputs: [], // empty for _reward_ tx
-    outputs: [{
-      address: wallet[0].pk,
-      value: config.blockReward
-    }],
-    nonce: cryptoUtils.randomBits()
-  }
+  // start the mining
+  miner({
+    blockReward: config.blockReward,
+    difficultyLevel: config.difficultyLevel,
+    pk: wallet[0].pk,
+    sk: wallet[0].sk
+  })
 
-  let lastBlock
-
-  // now we just mine blocks forever!
-  while (true) {
-    // the genesis block's header starts with all 0's
-    let hashPrevHeader = lastBlock
-      ? cryptoUtils.hash(lastBlock.header)
-      : '0'.repeat(64)
-
-    lastBlock = mineNewBlock(wallet[0].pk, wallet[0].sk, rewardTx, hashPrevHeader)
-    console.log(JSON.stringify(lastBlock))
-  }
 }
 
 main()
---
+++ miner.js
@@ -0,0 +1,47 @@
+const cryptoUtils = require('crypto-utils')
+const hashUtil = require('./hash-util')
+
+const mineNewBlock = (pk, sk, rewardTx, hashPrevHeader, difficultyLevel) => {
+  let blockHeader = {
+    hashPrevHeader: hashPrevHeader,
+    hashTxs: cryptoUtils.hash(rewardTx),
+    bits: difficultyLevel
+  }
+
+  blockHeader.nonce = hashUtil.generateProofOfWork(blockHeader, Number(difficultyLevel))
+
+  const block = {
+    header: blockHeader,
+    txs: [rewardTx],
+    signer: pk,
+    sig: cryptoUtils.sign({ header: blockHeader }, sk),
+    height: 0
+  }
+
+  return block
+}
+
+module.exports = ({ blockReward, difficultyLevel, pk, sk }) => {
+  // now let's create that reward tx
+  const rewardTx = {
+    inputs: [], // empty for _reward_ tx
+    outputs: [{
+      address: pk,
+      value: blockReward
+    }],
+    nonce: cryptoUtils.randomBits()
+  }
+
+  let lastBlock
+
+  // now we just mine blocks forever!
+  while (true) {
+    // the genesis block's header starts with all 0's
+    let hashPrevHeader = lastBlock
+      ? cryptoUtils.hash(lastBlock.header)
+      : '0'.repeat(64)
+
+    lastBlock = mineNewBlock(pk, sk, rewardTx, hashPrevHeader, difficultyLevel)
+    console.log(JSON.stringify(lastBlock))
+  }
+}
