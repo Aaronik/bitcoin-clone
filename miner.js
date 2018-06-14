aee03520af28f17606536960777c8f336ba6f0a2
Aaron Sullivan <aasullivan1618@gmail.com>
2018-06-14 14:50:08 -0700

    Add db rudiments

--- miner.js
+++ miner.js
@@ -2,6 +2,37 @@
 const cryptoUtils = require('crypto-utils')
 const hashUtil = require('./hash-util')
 
+// we're going to just keep a global db on this miner object for now.
+// this'll make for easy information retrieval.
+class Db {
+  constructor() {
+    this.blocks = []
+  }
+
+  // add a block to the db
+  addBlock(block) {
+    this.blocks.push(block)
+  }
+
+  // get all blocks from db
+  getBlocks() {
+    return this.blocks
+  }
+
+  // return total monetary supply created in blocks in db
+  getSupply() {
+    return this.blocks.reduce((supply, block) => {
+      // TODO this assumes reward transaction is first transaction in block
+      return supply + block.txs[0].outputs[0].value
+    }, 0)
+  }
+
+  // get all unspent transaction outputs
+  getUtxos() {
+    return []
+  }
+}
+
 // private helper methods
 const _mineNewBlock = (pk, sk, rewardTx, hashPrevHeader, difficultyLevel) => {
   let blockHeader = {
@@ -46,7 +77,7 @@ const initialize = ({ blockReward, difficultyLevel, pk, sk }) => {
       : '0'.repeat(64)
 
     lastBlock = _mineNewBlock(pk, sk, rewardTx, hashPrevHeader, difficultyLevel)
-    console.log(JSON.stringify(lastBlock))
+    console.log(lastBlock)
   }
 }
 
